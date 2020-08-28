c1986a326d0718001f0183873743e1e02ab7ec8e
reetesh_39 <reeteshkumar39@gmail.com>
2020-08-28 19:55:29 +0530

    added Room persistance

--- app/src/main/java/com/sbnri/ui/list/ListFragment.java
+++ app/src/main/java/com/sbnri/ui/list/ListFragment.java
@@ -6,12 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.paging.PagedList;
 import androidx.recyclerview.widget.LinearLayoutManager;
+
 import com.adapter.RepoItemAdapter;
 import com.sbnri.R;
 import com.sbnri.base.BaseApplication;
@@ -27,6 +29,7 @@ public class ListFragment extends BaseFragment {
     private ListViewModel listViewModel;
     FragmentListBinding binding;
     RepoItemAdapter adapter;
+
     @Override
     protected int layoutRes() {
         return R.layout.fragment_list;
@@ -34,7 +37,7 @@ public class ListFragment extends BaseFragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentListBinding.inflate(inflater,container, false);
+        binding = FragmentListBinding.inflate(inflater, container, false);
         listViewModel = new ViewModelProvider(this).get(ListViewModel.class);
         View view = binding.getRoot();
         binding.setLifecycleOwner(getActivity());
@@ -44,36 +47,31 @@ public class ListFragment extends BaseFragment {
         return view;
     }
 
-    private void getFeeds() {
-        if (InternetConnection.checkConnection(BaseApplication.get())) {
-            listViewModel.itemPagedList.observe(getViewLifecycleOwner(), new Observer<PagedList<RepoListItem>>() {
-                @Override
-                public void onChanged(PagedList<RepoListItem> repoListItems) {
-                    adapter.submitList(repoListItems);
-                    binding.progressBar.setVisibility(View.GONE);
-                }
-            });
-        } else {
-            listViewModel.getApiResponseLiveData().observe(getViewLifecycleOwner(), new Observer<List<RepoListItem>>() {
-                @Override
-                public void onChanged(@Nullable List<RepoListItem> locationData) {
-                    if (locationData != null) {
-                        Toast.makeText(getContext(),locationData.get(0).getName(),Toast.LENGTH_LONG).show();
-                    }
-                }
-            });
-        }
-
-
-
-    }
-
     private void initialization(View view) {
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         binding.feedListView.setLayoutManager(layoutManager);
         adapter = new RepoItemAdapter(getActivity());
         binding.feedListView.setAdapter(adapter);
+
+    }
+
+    private void getFeeds() {
+        if (InternetConnection.checkConnection(BaseApplication.get())) {
+            listViewModel.itemPagedList.observe(getViewLifecycleOwner(), repoListItems -> {
+                adapter.submitList(repoListItems);
+                binding.progressBar.setVisibility(View.GONE);
+            });
+        } else {
+            listViewModel.getApiResponseLiveData().observe(getViewLifecycleOwner(), locationData -> {
+                if (locationData != null) {
+                    Toast.makeText(getContext(), locationData.get(0).getName(), Toast.LENGTH_LONG).show();
+                    Toast.makeText(getContext(), "Please check network connection", Toast.LENGTH_LONG).show();
+
+                }
+            });
+        }
+
 
     }
 
